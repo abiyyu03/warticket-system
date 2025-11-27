5039160526e1a511f2642288a66aa506b1f54306
abiyyu03 <abiyyu652@gmail.com>
2025-11-27 09:35:28 +0700

    (refactor): improvement seats entity

--- internal/adapter/outbound/entity/seat.go
+++ internal/adapter/outbound/entity/seat.go
@@ -7,10 +7,14 @@ import (
 type (
 	Seat struct {
 		BaseModel
-		ID        int64  `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-		Code      string `gorm:"column:code;not null;uniqueIndex" json:"code"`
-		Available string `gorm:"column:available;not null;default:true" json:"available"`
-		Type      string `gorm:"column:type;not null;default:REGULAR" json:"type"` // VIP or REGULAR
+		ID         int64    `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+		Code       string   `gorm:"column:code;not null;uniqueIndex" json:"code"`
+		Row        int      `gorm:"column:row;not null" json:"row"`
+		Col        int      `gorm:"column:col;not null" json:"col"`
+		Available  string   `gorm:"column:available;not null;default:true" json:"available"`
+		Type       string   `gorm:"column:type;not null;default:REGULAR" json:"type"` // VIP or REGULAR
+		LocationID int64    `gorm:"column:location_id;not null" json:"location_id"`
+		Location   Location `gorm:"foreignKey:LocationID;references:ID" json:"location"`
 	}
 )
 
