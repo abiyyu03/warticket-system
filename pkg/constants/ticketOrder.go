67abbd640cebc7d895c29e714a4dfd6d1902a659
abiyyu03 <abiyyu652@gmail.com>
2026-08-20 21:24:21 +0700

    chore(constants): hapus ticketOrder yang tidak terpakai

--- pkg/constants/ticketOrder.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const (
-	TicketOrderInit      = "INIT_PAYMENT"
-	TicketOrderPending   = "PENDING_PAYMENT"
-	TicketOrderSuccess   = "SUCCESSFUL"
-	TicketOrderFailed    = "PAYMENT_FAILED"
-	TicketOrderCancelled = "PAYMENT_CANCELLED"
-)
